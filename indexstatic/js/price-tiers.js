d924d20a23a65fee7098d39fde694243b765fa7f
ByTanimsiz <kataahmet07@hotmail.com>
2024-03-01 00:23:50 +0300

    Add files via upload

---
+++ indexstatic/js/price-tiers.js
@@ -0,0 +1,29 @@
+//const oneMonthButton = document.getElementById("one-month-button");
+//const threeMonthButton = document.getElementById("three-month-button");
+//const priceTiers = document.getElementsByClassName("pricing-tier");
+//console.log(priceTiers[0].innerHTML);
+//
+//oneMonthButton.addEventListener("click", function () {
+//  console.log("clicked");
+//  priceTiers[0].innerHTML = "<span>€</span> 16.99";
+//  priceTiers[2].innerHTML = "<span>€</span> 21.99";
+//  priceTiers[1].innerHTML = "<span>€</span> 29.99";
+//  oneMonthButton.classList.add("bg-filled");
+//
+//  if (threeMonthButton.classList.contains("bg-filled")) {
+//    threeMonthButton.classList.remove("bg-filled");
+//  }
+//});
+//
+//threeMonthButton.addEventListener("click", function () {
+//  console.log("clicked");
+//  priceTiers[0].innerHTML = "<span>€</span> 34.99";
+//  priceTiers[2].innerHTML = "<span>€</span> 49.99";
+//  priceTiers[1].innerHTML = "<span>€</span> 54.99";
+//
+//  threeMonthButton.classList.add("bg-filled");
+//
+//  if (oneMonthButton.classList.contains("bg-filled")) {
+//    oneMonthButton.classList.remove("bg-filled");
+//  }
+//});
